127511e35f01a7904f6ee8ebce5b0e62edcbf049
Okeke Chukwuemeka Christian <okekechristian1960@yahoo.com>
2024-03-01 19:34:28 +0000

    Initial Staff Seeder

--- database/seeders/StaffSeeder.php
+++ database/seeders/StaffSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\School;
+use App\Models\Staff;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class StaffSeeder extends Seeder
@@ -12,6 +14,60 @@ class StaffSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $school = School::where('short_name', 'FUTO')->firstOrFail();
+
+        $user1 = User::updateOrCreate(
+            [
+                'email' => 'result-compiler@futo.com',
+            ],
+            [
+                'first_name' => 'Macqueen',
+                'last_name' => 'Chukwuma',
+                'phone_no' => '07033793094',
+                'is_staff' => true,
+                'password' => bcrypt('password'),
+            ]
+        );
+
+        $staff1 = Staff::updateOrCreate(
+            [
+                'user_id' => $user1->id,
+            ],
+            [
+                'school_id' => $school->id,
+                'date_of_entry' => '2008-09-21',
+            ]
+        );
+
+        $resultCompiler = Role::where('key', 'result-compiler')->firstOrFail();
+
+        $user1->roles()->attach($resultCompiler->id);
+
+        $user2 = User::updateOrCreate(
+            [
+                'email' => 'checking-compiler@futo.com',
+            ],
+            [
+                'first_name' => 'Joe',
+                'last_name' => 'Malaky',
+                'phone_no' => '07033793204',
+                'is_staff' => true,
+                'password' => bcrypt('password'),
+            ]
+        );
+
+        $staff2 = Staff::updateOrCreate(
+            [
+                'user_id' => $user2->id,
+            ],
+            [
+                'school_id' => $school->id,
+                'date_of_entry' => '2008-09-21',
+            ]
+        );
+
+        $checkingOfficer = Role::where('key', 'checking-officer')->firstOrFail();
+
+        $user2->roles()->attach($checkingOfficer->id);
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -24,33 +24,5 @@ class UserSeeder extends Seeder
         $user1->save();
 
         $superAdmin = Role::where('key', 'super-admin')->firstOrFail();
-
-        $user1->roles()->attach($superAdmin->id);
-
-        $user2 = new User;
-        $user2->first_name = 'Macqueen';
-        $user2->last_name = 'Chukwuma';
-        $user2->email = 'manager@rms.com';
-        $user2->phone_no = '07033793213';
-        $user1->is_staff = true;
-        $user2->password = bcrypt('password');
-        $user2->save();
-
-        $resultCompiler = Role::where('key', 'result-compiler')->firstOrFail();
-
-        $user2->roles()->attach($resultCompiler->id);
-
-        $user3 = new User;
-        $user3->first_name = 'Aruni';
-        $user3->last_name = 'Samuel';
-        $user3->email = 'staff@rms.com';
-        $user3->phone_no = '07033022383';
-        $user1->is_staff = true;
-        $user3->password = bcrypt('password');
-        $user3->save();
-
-        $checkingOfficer = Role::where('key', 'checking-officer')->firstOrFail();
-
-        $user3->roles()->attach($checkingOfficer->id);
     }
 }
